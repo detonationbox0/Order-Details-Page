efaca8f27300238e1fc2420ab8ec0fc86c6f88a4
Todd <detonationbox0@live.com>
2021-05-19 17:01:12 -0400

    Trying new phone detector

--- js/app.js
+++ js/app.js
@@ -388,14 +388,10 @@ $(function() {
     //#region
     console.log( "ready!" );
 
-    if( navigator.userAgent.match(/Android/i)
-    || navigator.userAgent.match(/webOS/i)
-    || navigator.userAgent.match(/iPhone/i)
-    || navigator.userAgent.match(/iPad/i)
-    || navigator.userAgent.match(/iPod/i)
-    || navigator.userAgent.match(/BlackBerry/i)
-    || navigator.userAgent.match(/Windows Phone/i) {
-        alert("This page is not optimized for mobile!")
+    var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+    if (mobile) {
+        alert("Visit this on a Computer for Better View");              
+    } else {
 
     }
 
